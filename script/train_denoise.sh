4da430fdd33815b16de20977975c599e444e5a29
Graph4HEP <bingzhili@sjtu.edu.cn>
2024-08-29 09:58:43 +0800

    update training script

--- script/train_denoise.sh
+++ script/train_denoise.sh
@@ -1,4 +1,4 @@
 python3 ./train/train_denoise.py --arch Uformer_B --batch_size 64 --gpu '0' \
-    --train_ps 128 --train_dir /root/autodl-tmp/training --env _first_train \
-    --val_dir /root/autodl-tmp/validation --save_dir ./logs/ \
+    --train_ps 128 --train_dir ../example_data/training --env _test \
+    --val_dir ../example_data/validation --save_dir ./logs/ \
     --dataset diffraction --warmup 
